363b48354ace3faad8a5b65c9e1b7f2d870bedc0
Xuewei Niu <justxuewei@apache.org>
2022-07-24 23:46:50 +0800

    feat(comment-app): init with react
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

---
+++ comment-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { Component, ReactNode } from 'react';
+
+class App extends Component {
+  render(): ReactNode {
+      return (
+        <h1>Hello, welcome to my first react app.</h1>
+      )
+  }
+}
+
+export default App;
