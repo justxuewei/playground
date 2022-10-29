2dddb6c4f9a24b6db2351eb77776c81d6b5f0b40
Xuewei Niu <justxuewei@apache.org>
2022-10-29 14:34:21 +0800

    feat(fp-rust): add binaryheap drain tests
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

--- fp-rust/src/iter_drain.rs
+++ fp-rust/src/iter_drain.rs
@@ -1,6 +1,33 @@
+use std::collections::BinaryHeap;
+
+/// Vec drain
 pub fn test_case_1() {
     let mut test_vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // test_vec = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     println!("test_vec = {:?}", test_vec);
     test_vec.drain(2..4);
+    // test_vec.drain(2..4) = [1, 2, 5, 6, 7, 8, 9, 10]
     println!("test_vec.drain(2..4) = {:?}", test_vec);
+}
+
+/// BinaryHeap drain
+pub fn test_case_2() {
+    let mut heap = BinaryHeap::<usize>::new();
+    heap.push(1);
+    heap.push(2);
+    heap.push(3);
+    heap.push(4);
+    heap.push(5);
+    // heap = [5, 4, 2, 1, 3]
+    println!("heap = {:?}", heap);
+    // 5
+    // 4
+    // 2
+    // 1
+    // 3
+    for i in heap.drain() {
+        println!("{}", i);
+    }
+    // heap = []
+    println!("heap = {:?}", heap);
 }
--- fp-rust/src/main.rs
+++ fp-rust/src/main.rs
@@ -4,4 +4,5 @@ mod iter_drain;
 fn main() {
     iter_take::test_case_1();
     iter_drain::test_case_1();
+    iter_drain::test_case_2();
 }
