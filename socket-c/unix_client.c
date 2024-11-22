a700572ca58bcd9815e06fbe052723ecb1111aa5
Xuewei Niu <justxuewei@apache.org>
2024-11-22 17:47:11 +0800

    socket-c: Add abstract UDS support for unix_{client&server}
    Now you can start a server listening at an abstract UDS path like:
    
    ```
    ./unix_server.out -a hello
    ```
    
    The client usage is similar to the above.
    
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

--- socket-c/unix_client.c
+++ socket-c/unix_client.c
@@ -9,49 +9,68 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
-        printf("Usage: %s {socket_path}\n", argv[0]);
-        return 1;
-    }
+	char path[108];
+	int use_abs_path, sockfd, n;
+	struct sockaddr_un server_addr;
+	char buf[BUFSIZE];
 
-    int sockfd, n;
-    struct sockaddr_un server_addr;
-    char buf[BUFSIZE];
+	if (argc == 2) {
+		strcpy(path, argv[1]);
+		use_abs_path = 0;
+	} else if (argc == 3) {
+		if (strcmp(argv[1], "-a"))
+			goto out;
+		use_abs_path = 1;
+		strcpy(path, argv[2]);
+	} else {
+		goto out;
+	}
 
-    if ((sockfd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        return sockfd;
-    }
+	if ((sockfd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+		perror("socket failed");
+		return sockfd;
+	}
 
-    memset(&server_addr, 0, sizeof(struct sockaddr_un));
-    server_addr.sun_family = AF_UNIX;
-    strncpy(server_addr.sun_path, argv[1], sizeof(server_addr.sun_path) - 1);
+	memset(&server_addr, 0, sizeof(struct sockaddr_un));
+	server_addr.sun_family = AF_UNIX;
+	if (use_abs_path) {
+		strncpy(server_addr.sun_path + 1, path,
+			sizeof(server_addr.sun_path) - 2);
+	} else {
+		strncpy(server_addr.sun_path, path,
+			sizeof(server_addr.sun_path) - 1);
+	}
 
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_un)) < 0) {
-        perror("connect failed");
-        close(sockfd);
-        return -1;
-    }
+	if (connect(sockfd, (struct sockaddr *)&server_addr,
+		    sizeof(struct sockaddr_un)) < 0) {
+		perror("connect failed");
+		close(sockfd);
+		return -1;
+	}
 
-    memset(buf, 0, BUFSIZE);
-    strcpy(buf, "hello from client!");
-    n = send(sockfd, buf, strlen(buf), 0);
-    if (n < 0) {
-        perror("send failed");
-        close(sockfd);
-        return n;
-    }
-    printf("Sent %d bytes to server:\n%s\n", n, buf);
+	memset(buf, 0, BUFSIZE);
+	strcpy(buf, "hello from client!");
+	n = send(sockfd, buf, strlen(buf), 0);
+	if (n < 0) {
+		perror("send failed");
+		close(sockfd);
+		return n;
+	}
+	printf("Sent %d bytes to server:\n%s\n", n, buf);
 
-    memset(buf, 0, BUFSIZE);
-    n = recv(sockfd, buf, BUFSIZE, 0);
-    if (n < 0) {
-        perror("recv failed");
-        close(sockfd);
-        return n;
-    }
-    printf("Received %d bytes from server:\n%s\n", n, buf);
+	memset(buf, 0, BUFSIZE);
+	n = recv(sockfd, buf, BUFSIZE, 0);
+	if (n < 0) {
+		perror("recv failed");
+		close(sockfd);
+		return n;
+	}
+	printf("Received %d bytes from server:\n%s\n", n, buf);
 
-    close(sockfd);
-    return 0;
+	close(sockfd);
+	return 0;
+
+out:
+	printf("Usage: %s [-a] {socket_path}\n", argv[0]);
+	return -1;
 }
--- socket-c/unix_server.c
+++ socket-c/unix_server.c
@@ -9,70 +9,97 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
-        printf("Usage: %s {socket_path}\n", argv[0]);
-        return 1;
-    }
+	int use_abs_path, server_fd, client_fd, n;
+	char path[108];
+	struct sockaddr_un server_addr, client_addr;
+	char buf[BUFSIZE];
+	socklen_t client_addr_len = sizeof(client_addr);
 
-    int server_fd, client_fd, n;
-    struct sockaddr_un server_addr, client_addr;
-    char buf[BUFSIZE];
-    socklen_t client_addr_len = sizeof(client_addr);
+	if (argc == 2) {
+		use_abs_path = 0;
+		strcpy(path, argv[1]);
+	} else if (argc == 3) {
+		if (strcmp(argv[1], "-a"))
+			goto out;
+		use_abs_path = 1;
+		strcpy(path, argv[2]);
+	} else {
+		goto out;
+	}
 
-    if ((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        return server_fd;
-    }
+	if ((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+		perror("socket failed");
+		return server_fd;
+	}
 
-    memset(&server_addr, 0, sizeof(struct sockaddr_un));
-    server_addr.sun_family = AF_UNIX;
-    strncpy(server_addr.sun_path, argv[1], sizeof(server_addr.sun_path) - 1);
+	memset(&server_addr, 0, sizeof(struct sockaddr_un));
+	server_addr.sun_family = AF_UNIX;
+	if (use_abs_path) {
+		server_addr.sun_path[0] = 0;
+		strncpy(server_addr.sun_path + 1, path,
+			sizeof(server_addr.sun_path) - 2);
+		server_addr.sun_path[strlen(path) + 2] = 0;
+	} else {
+		strncpy(server_addr.sun_path, path,
+			sizeof(server_addr.sun_path) - 1);
+	}
 
-    unlink(argv[1]); // 删除已存在的 socket 文件
+	if (!use_abs_path)
+		unlink(path); // 删除已存在的 socket 文件
 
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_un)) < 0) {
-        perror("bind failed");
-        close(server_fd);
-        return -1;
-    }
+	if (bind(server_fd, (struct sockaddr *)&server_addr,
+		 sizeof(struct sockaddr_un)) < 0) {
+		perror("bind failed");
+		close(server_fd);
+		return -1;
+	}
 
-    if (listen(server_fd, 5) < 0) {
-        perror("listen failed");
-        close(server_fd);
-        return -1;
-    }
+	if (listen(server_fd, 5) < 0) {
+		perror("listen failed");
+		close(server_fd);
+		return -1;
+	}
 
-    printf("Server is listening on %s\n", argv[1]);
+	if (use_abs_path)
+		printf("Server is listening on @%s\n", path);
+	else
+		printf("Server is listening on %s\n", path);
 
-    if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0) {
-        perror("accept failed");
-        close(server_fd);
-        return -1;
-    }
+	if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+				&client_addr_len)) < 0) {
+		perror("accept failed");
+		close(server_fd);
+		return -1;
+	}
 
-    memset(buf, 0, BUFSIZE);
-    n = recv(client_fd, buf, BUFSIZE, 0);
-    if (n < 0) {
-        perror("recv failed");
-        close(client_fd);
-        close(server_fd);
-        return -1;
-    }
-    printf("Received %d bytes from client:\n%s\n", n, buf);
+	memset(buf, 0, BUFSIZE);
+	n = recv(client_fd, buf, BUFSIZE, 0);
+	if (n < 0) {
+		perror("recv failed");
+		close(client_fd);
+		close(server_fd);
+		return -1;
+	}
+	printf("Received %d bytes from client:\n%s\n", n, buf);
 
-    strcpy(buf, "hello from server!");
-    n = send(client_fd, buf, strlen(buf), 0);
-    if (n < 0) {
-        perror("send failed");
-        close(client_fd);
-        close(server_fd);
-        return n;
-    }
-    printf("Sent %d bytes to client:\n%s\n", n, buf);
+	strcpy(buf, "hello from server!");
+	n = send(client_fd, buf, strlen(buf), 0);
+	if (n < 0) {
+		perror("send failed");
+		close(client_fd);
+		close(server_fd);
+		return n;
+	}
+	printf("Sent %d bytes to client:\n%s\n", n, buf);
 
-    close(client_fd);
-    close(server_fd);
-    unlink(argv[1]); // 删除 socket 文件
+	close(client_fd);
+	close(server_fd);
+	if (!use_abs_path)
+		unlink(path); // 删除 socket 文件
 
-    return 0;
+	return 0;
+
+out:
+	printf("Usage: %s [-a] {socket_path}\n", argv[0]);
+	return -1;
 }
