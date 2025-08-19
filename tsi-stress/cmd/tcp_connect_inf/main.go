f0164d48e50c03b537711ad3f004cbc164103946
Xuewei Niu <justxuewei@apache.org>
2025-08-19 10:53:26 +0800

    tsi-stress: Check TSI status before TCP connect test
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

--- tsi-stress/cmd/tcp_connect_inf/main.go
+++ tsi-stress/cmd/tcp_connect_inf/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"log"
 	"net"
+	"os"
+	"strings"
 )
 
 func connect() {
@@ -9,6 +12,15 @@ func connect() {
 }
 
 func main() {
+	data, err := os.ReadFile("/proc/cmdline")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmdline := string(data)
+	if !strings.Contains(cmdline, "tsi_hijack=") {
+		log.Fatal("TSI not enabled")
+	}
+
 	for {
 		go connect()
 	}
